03e7c7ab89f8f8d51bba4cdadd7a87d5313d8de5
Daniil <eo18622@yandex.ru>
2023-12-19 10:17:03 +0300

    схема 2

--- shema.sql
+++ shema.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE mnogoruba;
 
-USE mnogoruba CREATE TABLE `user` (
+USE mnogoruba;
+
+CREATE TABLE `user` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `date_reg` DATETIME DEFAULT CURRENT_TIMESTAMP,
     `telephone` VARCHAR(20) UNIQUE,
@@ -19,8 +21,18 @@ CREATE TABLE `menu` (
     `description` VARCHAR(255),
     `price` INT,
     `cooking_time` INT,
-    `category__id` INT,
-    FOREIGN KEY (`category__id`) REFERENCES `category_menu` (`id`)
+    `category__name` ENUM(
+        'поке',
+        'роллы',
+        'супы',
+        'горячее',
+        'вок',
+        'закуски',
+        'сэндвичи',
+        'десерты',
+        'напитки',
+        'соус'
+    )
 );
 
 CREATE TABLE `category_menu` (
@@ -36,7 +48,14 @@ CREATE TABLE `category_menu` (
         'десерты',
         'напитки',
         'соус'
-    ),
+    )
+);
+
+CREATE TABLE `order` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `order_time` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `menu_id` INT,
+    FOREIGN KEY (`menu_id`) REFERENCES `menu` (`id`)
 );
 
 CREATE TABLE `component` (
@@ -70,9 +89,8 @@ CREATE TABLE `poke` (
     `price` INT
 );
 
-CREATE TABLE `order` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `order_time` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `menu_id` INT,
-    FOREIGN KEY (`menu_id`) REFERENCES `menu` (`id`)
-);
+-- Добавляем внешний ключ после создания таблицы `category_menu`
+ALTER TABLE
+    `menu`
+ADD
+    FOREIGN KEY (`category__name`) REFERENCES `category_menu` (`category__name`);
