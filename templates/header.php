7ec2988ab2b016473e8256632cb25278e3a2b2cc
Daniil <eo18622@yandex.ru>
2024-01-11 15:50:12 +0300

    шаблон главной страницы готова

---
+++ templates/header.php
@@ -0,0 +1,49 @@
+<!-- Хедер -->
+<header class="header">
+    <div class="header__wrapper">
+        <a class="header__logo-link" href="#">
+            <img src="./img/assets/logo.svg" alt="" class="header__logo">
+        </a>
+
+        <div class="header__burger">
+            <span class="header__burger-line"></span>
+            <span class="header__burger-line"></span>
+            <span class="header__burger-line"></span>
+            <span class="header__burger-line"></span>
+        </div>
+
+        <nav class="header__nav">
+            <ul class="header__nav-list">
+                <li class="header__nav-item">
+                    <a class="header__nav-item-link" href="#">
+                        Главная
+                    </a>
+                </li>
+
+                <li class="header__nav-item">
+                    <a class="header__nav-item-link" href="#">
+                        Регистрация
+                    </a>
+                </li>
+
+                <li class="header__nav-item">
+                    <a class="header__nav-item-link" href="#">
+                        Личный кабинет
+                    </a>
+                </li>
+
+                <li class="header__nav-item">
+                    <a class="header__nav-item-link" href="#">
+                        Корзина
+                    </a>
+                </li>
+
+                <li class="header__nav-item">
+                    <a class="header__nav-item-link" href="#">
+                        Меню
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
---
+++ templates/main.php
@@ -0,0 +1,123 @@
+<!-- Главная страница -->
+<div class="page__main">
+    <!-- Предложение -->
+    <div class="offer">
+        <div class="offer__wrapper">
+            <button class="offer__button" type="button">
+                Меню
+            </button>
+
+            <button class="offer__button" type="button">
+                Создать поке
+            </button>
+        </div>
+    </div>
+
+
+    <!-- Баннер -->
+    <div class="banner">
+        <div class="banner__wrapper">
+            <img src="./img/assets/banner-1.jpg" alt="" class="banner__img">
+        </div>
+    </div>
+
+
+    <!-- Меню -->
+    <div class="menu">
+        <div class="menu__wrapper">
+            <h2 class="title menu__title">
+                Популярные блюда
+            </h2>
+
+            <ul class="menu__list">
+                <li class="menu__item">
+                    <img src="https://102922.selcdn.ru/nomenclature_images_test/1926609/7a849eb1-7100-4d77-bde2-d6bac3002d46.jpg" alt="" class="menu__item-img">
+
+                    <div class="menu__item-content">
+                        <h3 class="sub-title menu__item-title">
+                            Поке микс Лосось-Тунец
+                        </h3>
+
+                        <p class="text menu__item-text">
+                            Лосось, тунец, соус дрессинг, рис, грибы, битые огурцы, азиатская морковь, салат
+                            айсберг,...
+                        </p>
+
+                        <div class="menu__item-info">
+                            <p class="text menu__item-price">
+                                410
+                            </p>
+
+                            <div class="menu__item-info-order">
+                                <button class="menu__item-button hidden" type="button">
+                                    В корзину
+                                </button>
+
+                                <div class="basket__item-counter">
+                                    <span class="basket__item-action basket__item-action--minus">
+                                        –
+                                    </span>
+
+                                    <p class="basket__item-count">
+                                        10
+                                    </p>
+
+                                    <span class="basket__item-action basket__item-action--plus">
+                                        +
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+
+            <nav class="menu__nav">
+                <button class="menu__nav-button menu__nav-button--prev">
+                    <svg class="menu__nav-button-img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+                        <path stroke="currentColor" stroke-width="2px" stroke-linecap="square" d="m26.1 46-2-2 11.8-11.7-11.8-11.7 2-2 13.8 13.7L26.1 46" />
+                    </svg>
+                </button>
+
+                <ul class="menu__nav-list">
+                    <li class="menu__nav-item">
+                        1
+                    </li>
+
+                    <li class="menu__nav-item">
+                        2
+                    </li>
+
+                    <li class="menu__nav-item">
+                        3
+                    </li>
+                </ul>
+
+                <button class="menu__nav-button menu__nav-button--next">
+                    <svg class="menu__nav-button-img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+                        <path stroke="currentColor" stroke-width="2px" stroke-linecap="square" d="m26.1 46-2-2 11.8-11.7-11.8-11.7 2-2 13.8 13.7L26.1 46" />
+                    </svg>
+                </button>
+            </nav>
+        </div>
+    </div>
+
+
+    <!-- О компании -->
+    <div class="about">
+        <div class="about__wrapper">
+            <img src="../../img/assets/about-1.png" alt="" class="about__img">
+
+            <h2 class="title about__title">
+                О компании
+            </h2>
+
+            <p class="text about__text">
+                Здоровый и вкусный завтрак, быстрый обед или ужин со свежими и экзотическими ароматами. В "Много
+                Рыбы" у нас есть все! Ознакомьтесь с нашим меню, разработанным искусными шеф-поварами с
+                использованием настоящих сезонных ингредиентов, и полюбите вкуснейший Poke Bowl. Либо выберите
+                одно из наших фирменных блюд, либо создайте свой идеальный Poke!
+            </p>
+        </div>
+    </div>
+</div>
