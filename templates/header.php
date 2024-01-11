c2d37eb045e089699d6c3d05bcde1bb93fb13b44
Daniil <eo18622@yandex.ru>
2024-01-11 16:10:22 +0300

    навигация по страницам готова

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <!-- Хедер -->
 <header class="header">
     <div class="header__wrapper">
-        <a class="header__logo-link" href="#">
+        <a class="header__logo-link" href="../project/index.php">
             <img src="./img/assets/logo.svg" alt="" class="header__logo">
         </a>
 
@@ -15,31 +15,31 @@
         <nav class="header__nav">
             <ul class="header__nav-list">
                 <li class="header__nav-item">
-                    <a class="header__nav-item-link" href="#">
+                    <a class="header__nav-item-link" href="../project/index.php">
                         Главная
                     </a>
                 </li>
 
                 <li class="header__nav-item">
-                    <a class="header__nav-item-link" href="#">
+                    <a class="header__nav-item-link" href="../project/reg.php">
                         Регистрация
                     </a>
                 </li>
 
                 <li class="header__nav-item">
-                    <a class="header__nav-item-link" href="#">
+                    <a class="header__nav-item-link" href="../project/account.php">
                         Личный кабинет
                     </a>
                 </li>
 
                 <li class="header__nav-item">
-                    <a class="header__nav-item-link" href="#">
+                    <a class="header__nav-item-link" href="../project/basket.php">
                         Корзина
                     </a>
                 </li>
 
                 <li class="header__nav-item">
-                    <a class="header__nav-item-link" href="#">
+                    <a class="header__nav-item-link" href="../project/menu.php">
                         Меню
                     </a>
                 </li>
