022b0d48a5161b8704785df7f94ae5bd8d3b38f4
Daniil <eo18622@yandex.ru>
2024-01-11 16:06:39 +0300

    страницы магазина готовы

---
+++ account.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once('./functions/helpers.php');
+
+
+$page_head = include_template(
+    'head.php',
+    [
+        'title' => 'poke-room «Много рыбы»',
+    ]
+);
+
+$page_header = include_template(
+    'header.php',
+    []
+);
+
+$page_body = include_template(
+    'account.php',
+    []
+);
+
+$page_footer = include_template(
+    'footer.php',
+    []
+);
+
+$layout_content = include_template(
+    'layout.php',
+    [
+        'head' => $page_head,
+        'header' => $page_header,
+        'main' => $page_body,
+        'footer' => $page_footer,
+    ]
+);
+
+print($layout_content);
