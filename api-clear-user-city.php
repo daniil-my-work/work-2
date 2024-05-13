c82f40bfb8b7f7cbc8473a692aa41b48c442bb48
Daniil <eo18622@yandex.ru>
2024-05-13 10:32:46 +0300

    стили для страницы Корзина

--- api-clear-user-city.php
+++ api-clear-user-city.php
@@ -4,4 +4,9 @@ require_once('./functions/init.php');
 
 unset($_SESSION['city']);
 
-header("Location: ./index.php");
+if (isset($_SERVER['HTTP_REFERER'])) {
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+} else {
+    // Запасной вариант, если HTTP_REFERER не доступен
+    header('Location: ./index.php');
+}
